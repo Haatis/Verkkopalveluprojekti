302a0bdb6a2d61cac948460a16318660d29b70b8
JoonaHaataja <joona00@windowslive.com>
2021-03-16 10:33:22 +0200

    singlepageapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {Switch, Route} from 'react-router-dom'
+import Home from './Home';
+import About from './About';
+import NotFound from './NotFound';
+import ContactUs from './ContactUs';
+import NavBar from './NavBar';
+import Header from './Header';
+import Footer from './Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+   <NavBar />
+   <Header />
+   <div className="container">
+     <Switch>
+       <Route path="/" component={Home} exact />
+       <Route path="/about" component={About} />
+       <Route path="/contactus" component={ContactUs} />
+       <Route component={NotFound} />
+     </Switch>
+   </div>
+   <Footer />
+   </>
   );
 }
 
---
+++ src/ContactUs.js
@@ -0,0 +1,7 @@
+export default function ContactUs() {
+    return (
+        <div>
+            This is contact Page
+        </div>
+    )
+}
