8c1e001e866d7bbe06eb993b578b782c0baf31cf
Joni Karvonen <n9kajo01@students.oamk.fi>
2021-03-18 14:24:48 +0200

    tuote card

--- src/Footer.js
+++ src/Footer.js
@@ -3,10 +3,8 @@ import React from 'react'
 
 export default function Footer() {
     return (
-        <footer className="footer">
-            <div className="container">
-                <span className="text-muted">Comm's</span>
-            </div>
+        <footer>
+            Comm's
         </footer>
     )
 }
