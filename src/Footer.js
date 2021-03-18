a702dd85ca450d30f3a30dd117bd2d6f0ba03d8f
n0puro00 <n0puro00@students.oamk.fi>
2021-03-18 12:34:17 +0200

    footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,7 +5,7 @@ export default function Footer() {
     return (
         <footer className="footer">
             <div className="container">
-                <span className="text-muted">Place sticky footer content here.</span>
+                <span className="text-muted">Comm's</span>
             </div>
         </footer>
     )
