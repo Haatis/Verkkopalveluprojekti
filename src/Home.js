9d52e7fd03bd4375f8dea77f3699f33e6af9e240
JoonaHaataja <joona00@windowslive.com>
2021-03-18 14:26:17 +0200

    asd

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,20 @@
 export default function Home() {
     return (
+        <>
+
         <div>
-           <h1>This is Home Page</h1>
+           <h1>Päivän tarjoukset</h1>
         </div>
+        <div class="card" >
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+    </div>
+        <div>
+        <h1>Comm's Suosittelee</h1>
+     </div>
+     </>
     )
 }
