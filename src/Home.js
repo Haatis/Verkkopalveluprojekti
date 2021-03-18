e331fd38bb39e381b4cd64f5d6f96215cc36562f
JoonaHaataja <joona00@windowslive.com>
2021-03-18 15:03:31 +0200

    etusivu tyylittelyä ja pieniä muutoksia

--- src/Home.js
+++ src/Home.js
@@ -2,7 +2,7 @@ export default function Home() {
     return (
         <>
 
-        <div>
+        <div class="row">
            <h1>Päivän tarjoukset</h1>
         </div>
 
@@ -60,60 +60,8 @@ export default function Home() {
                 </div>
         </div>
 
-        <div>
+        <div class="row">
         <h1>Comm's Suosittelee</h1>
-        <div class="row">
-        <div class="card col-3">
-                    <a href="">
-                        <div >
-                            <img src="" class="card-img-top" alt=""></img>
-                            <div class="card-body">
-                                <h5 class="card-title">Card with stretched link</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="card col-3">
-                    <a href="">
-                        <div>
-                            <img src="" class="card-img-top" alt=""></img>
-                            <div class="card-body">
-                                <h5 class="card-title">Card with stretched link</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="card col-3">
-                    <a href="">
-                        <div>
-                            <img src="" class="card-img-top" alt=""></img>
-                            <div class="card-body">
-                                <h5 class="card-title">Card with stretched link</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-                <div class="card col-3">
-                    <a href="">
-                        <div>
-                            <img src="" class="card-img-top" alt=""></img>
-                            <div class="card-body">
-                                <h5 class="card-title">Card with stretched link</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </a>
-                </div>
         </div>
         <div class="row">
         <div class="card col-3">
@@ -168,7 +116,60 @@ export default function Home() {
                     </a>
                 </div>
         </div>
-     </div>
+        <div class="row">
+        <div class="card col-3">
+                    <a href="">
+                        <div >
+                            <img src="" class="card-img-top" alt=""></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card with stretched link</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="card col-3">
+                    <a href="">
+                        <div>
+                            <img src="" class="card-img-top" alt=""></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card with stretched link</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="card col-3">
+                    <a href="">
+                        <div>
+                            <img src="" class="card-img-top" alt=""></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card with stretched link</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="card col-3">
+                    <a href="">
+                        <div>
+                            <img src="" class="card-img-top" alt=""></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card with stretched link</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+        </div>
+     
      </>
     )
 }
