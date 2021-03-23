46268e88a637ce21cb29196389ea126f59f081f9
Joni Karvonen <n9kajo01@students.oamk.fi>
2021-03-23 12:10:21 +0200

    tuotteiden näyttäminen front-endissä

--- src/Content.js
+++ src/Content.js
@@ -1,45 +1,95 @@
+import { useState, useEffect } from "react";
+
 export default function Content() {
+    const URL = "http://localhost/verkkokauppa/";
+    const [tuotenimi, setTuotenimi] = useState("");
+    const [hinta, setHinta] = useState("");
+    const [tuotekuvaus, setTuotekuvaus] = useState("");
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        let status = 0;
+        fetch(URL + "index.php")
+            .then((response) => {
+                status = parseInt(response.status);
+                return response.json();
+            })
+            .then(
+                (response) => {
+                    if (status === 200) {
+                        setItems(response);
+                    } else {
+                        alert(response.error);
+                    }
+                },
+                (error) => {
+                    alert(error);
+                }
+            );
+    }, []);
+
     return (
-        <div className="row">
-            <div className="col-3 bg-secondary border border-dark">
-                <h1>sidebar</h1>
-                <div>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                        <button class="btn bt text-dark bg-light" type="submit">Hae</button>
-                    </form>
-                </div>
-                <div>
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Lajittele</option>
-                        <option value="1">One</option>
-                        <option value="2">Two</option>
-                        <option value="3">Three</option>
-                    </select>
-                </div>
+        <>
+            <div className="row">
 
+
+                <div className="col-3 bg-secondary border border-dark">
+                    <h1>Haku</h1>
+                    <div>
+                        <form className="d-flex">
+                            <input
+                                className="form-control me-2"
+                                type="search"
+                                placeholder="Search"
+                                aria-label="Search"
+                            />
+                            <button className="btn bt text-dark bg-light" type="submit">
+                                Hae
+              </button>
+                        </form>
+                    </div>
+                    <div>
+                        <select className="form-select" aria-label="Default select example">
+                            <option selected>Lajittele</option>
+                            <option value="1">One</option>
+                            <option value="2">Two</option>
+                            <option value="3">Three</option>
+                        </select>
+                    </div>
+                </div>
+                <div className="col-9 bg-secondary border border-dark">
+                    <h1>content</h1>
+
+
+                    <ul className="row">
+                        {items.map((item) => (
+                            <li className="card col-3 my-2 mx-4 p-1" key={item.id}>
+                                <div>
+                                    <a href="">
+                                        <div >
+                                            <img src="" class="card-img-top" alt=""></img>
+                                            <div class="card-body">
+                                                <img src="" className="" alt="Logo" />
+                                                <h5 class="card-title">{item.tuotenimi}</h5>
+                                                <p class="card-text">{item.tuotenimi}</p>
+                                                <p class="card-text">{item.tuotekuvaus}</p>
+                                                <div class="vasen-pohja">
+                                                    <a href="#" class="btn btn-primary"><i class="fa fa-shopping-cart"></i></a>
+                                                </div>
+                                                <p>{item.hinta + "€"}</p>
+                                            </div>
+                                        </div>
+                                    </a>
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+
+
+
+
+                </div>
             </div>
-            <div className="col-9 bg-secondary border border-dark">
-                <h1>content</h1>
-
-                <div>
-                    <a href="">
-                        <div class="card col-3">
-                            <img src="" class="card-img-top" alt=""></img>
-                            <div class="card-body">
-                                <h5 class="card-title">Card with stretched link</h5>
-                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-
-
-
-
-
-            </div>
-        </div>
-    )
+        </>
+    );
 }
