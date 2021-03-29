22da2a86b30c56688ec5e23287366fca93af9677
Joni Karvonen <n9kajo01@students.oamk.fi>
2021-03-29 19:09:55 +0300

    ostoskoriin lisääminen search ja product sivuilla

--- src/Search.js
+++ src/Search.js
@@ -8,6 +8,7 @@ export default function Search() {
   const [tuotekuvaus, setTuotekuvaus] = useState("");
   const [kuva, setTuotekuva] = useState("");
   const [items, setItems] = useState([]);
+  const [cart, setCart] = useState([]);
 
   /*
   useEffect(() => {
@@ -33,48 +34,56 @@ export default function Search() {
   }, []);
 */
 
+  useEffect(() => {
+    if ("cart" in localStorage) {
+      setCart(JSON.parse(localStorage.getItem("cart")));
+    }
+  }, []);
+
+  function addToCart(item) {
+    const newCart = [...cart, item];
+    setCart(newCart);
+    localStorage.setItem("cart", JSON.stringify(newCart));
+    window.location.reload(false);
+  }
+
   function searchItem(e) {
     e.preventDefault();
     window.location.href = "http://localhost:3000/search?query=" + search;
   }
 
-  const queryString = require('query-string');
-const parsed = queryString.parse(window.location.search);
-
+  const queryString = require("query-string");
+  const parsed = queryString.parse(window.location.search);
 
   useEffect(() => {
-        let status = 0;
-        fetch(URL + "search.php", {
-          method: "POST",
-          headers: {
-            Accept: "application/json",
-            "Content-type": "application/json",
-          },
-          body: JSON.stringify({
-            search: parsed.query,
-          }),
-        })
-          .then((response) => {
-            status = parseInt(response.status);
-            return response.json();
-          })
-          .then(
-            (response) => {
-              if (status === 200) {
-                setItems(response);
-              } else {
-                alert(response.error);
-              }
-            },
-            (error) => {
-              alert(error);
-            }
-          );
-      }, []);
-
-    
-
-
+    let status = 0;
+    fetch(URL + "search.php", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        search: parsed.query,
+      }),
+    })
+      .then((response) => {
+        status = parseInt(response.status);
+        return response.json();
+      })
+      .then(
+        (response) => {
+          if (status === 200) {
+            setItems(response);
+          } else {
+            alert(response.error);
+          }
+        },
+        (error) => {
+          alert(error);
+        }
+      );
+  }, []);
 
   return (
     <>
@@ -89,7 +98,10 @@ const parsed = queryString.parse(window.location.search);
               value={search}
               onChange={(e) => setSearch(e.target.value)}
             />
-            <button className="btn bt text-light btn-primary my-2 py-0 " type="submit">
+            <button
+              className="btn bt text-light btn-primary my-2 py-0 "
+              type="submit"
+            >
               Search
             </button>
           </form>
@@ -105,32 +117,37 @@ const parsed = queryString.parse(window.location.search);
         <div className="col-9 bg-secondary border-top border-bottom border-start  border-dark">
           <h1>content</h1>
 
-          <ul className="row">
+          <div className="row">
             {items.map((item) => (
-              <li className="card col-lg-6 col-md-12 text-center" key={item.id}>
-                <div>
-                  <a href="">
-                    <div>
-                      <img src="" className="card-img-top" alt=""></img>
-                      <div className="card-body">
-                        <img src={item.kuva} className="tuotekuva" alt="Logo" />
-                        <h5 className="card-title">{item.tuotenimi}</h5>
-                        <p className="card-text">{item.tuotenimi}</p>
-                        <p className="card-text">{item.tuotekuvaus}</p>
-                        <p className="card-text">{item.tuotetiivistelmä}</p>
-                        <div className="vasen-pohja">
-                          <a href="#" className="btn btn-primary">
-                            <i className="fa fa-shopping-cart"></i>
-                          </a>
-                        </div>
+              <div
+                className="card col-xl-3 col-lg-6 col-md-6 col-sm-12 p-0 text-center"
+                key={item.id}
+              >
+                <a /* href={"/Product/" + item.id} */>
+                  <div>
+                    <img src="" className="card-img-top" alt=""></img>
+                    <div className="card-body">
+                      <img src={item.kuva} className="img-fluid" alt="Logo" />
+                      <h5 className="card-title">{item.tuotenimi}</h5>
+                      <p className="card-text text-left">{item.tuotekuvaus}</p>
+                      <div className="vasen-pohja">
+                        <button
+                          onClick={() => addToCart(item.id)}
+                          value={item.id}
+                          className="btn btn-primary"
+                        >
+                          <i className="fa fa-shopping-cart"></i>
+                        </button>
+                      </div>
+                      <div className="oikea-pohja">
                         <p>{item.hinta + "€"}</p>
                       </div>
                     </div>
-                  </a>
-                </div>
-              </li>
+                  </div>
+                </a>
+              </div>
             ))}
-          </ul>
+          </div>
         </div>
       </div>
     </>
