64f7189053f8a26b31fd3ca2dc30a1ff3eeb89ae
JoonaHaataja <joona00@windowslive.com>
2021-03-30 20:37:14 +0300

    asd

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,7 @@ export default function Login() {
   const [loginStatus, setLoginStatus] = useState('');
   const [user, setUser] = useState([])
 
+
     const login = (e) => {
         e.preventDefault();
         axios.post('http://localhost/verkkokauppa/login.php', {
@@ -18,11 +19,10 @@ export default function Login() {
         }).then((response) => {
 
           if ((response.data.id)>0) {
-            setLoginStatus(response.data.id)
             addToUser(response.data.username)
-            
           } else {
             setLoginStatus("hommat kusi")
+            
             
           }
              console.log(response.data);
@@ -46,49 +46,43 @@ export default function Login() {
     localStorage.clear("user")
     window.location.reload(false);
   }
+  
 
   return (
-    <div>
-      
+    <>
+    
 
-      <form className="bg-light">
-        <div className="row">
+    <form className="bg-light">
+          <div className="row">
           <label for="exampleEmail" sm={2}>Username</label>
           <div className="col-sm-10" >
             <input onChange={(e) => setUsername(e.target.value)} type="text" required/>
           </div>
         </div>
-
         <div className="row">
           <label for="examplePassword" sm={2}>Password</label>
           <div className="col-sm-10" >
             <input onChange={(e) => setPassword(e.target.value)} type="text" required/>
           </div>
         </div>
-
         <div className="row">
         <div className="col-sm-10" >
             <button type="submit" value="Submit" onClick={login}>kirjaudu</button>
-            <a className="ms-5" href="./register">Luo uusi käyttäjä</a>
-            <button className="ms-5" type="button" onClick={() => emptyUser()}>Kirjaudu ulos</button>
+             <a className="ms-5" href="./register">Luo uusi käyttäjä</a>
           </div>
         </div>
         <h1>
-       
       </h1>
-        <div className="row">
-        <div className="col-sm-10" >
-            
-          </div>
-        </div>
-        <h1>{user}</h1>
+      
+      <div>
+    <h1>{user}</h1>
+    <h1>{loginStatus}</h1>
+    {("user" in localStorage) &&<button className="ms-5" type="button" onClick={() => emptyUser()}>Kirjaudu ulos</button>}
+   </div>
+
       </form>
+   </>
 
-        
-
-    </div>
-
-    
   );
 }
 
