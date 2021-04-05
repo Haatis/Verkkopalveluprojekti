a9ad217d1e3261580aa72d4ee7dacf89fed18962
JoonaHaataja <joona00@windowslive.com>
2021-04-05 08:52:28 +0300

    arviointi/kommunikointi (alkuvaiheessa)

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import axios from 'axios';  
 
 export default function Product() {
     const URL = "HTTP://localhost/verkkokauppa/"
@@ -9,8 +10,14 @@ export default function Product() {
     const [tuotekuvaus, setTuotekuvaus] = useState("");
     const [kuva, setTuotekuva] = useState("");
     const [items, setItems] = useState([]);
+    const [kommentit, setKommentit] = useState([]);
     const { it } = useParams();
     const [cart, setCart] = useState([]);
+    const [otsikko, setOtsikko] = useState('');
+    const [kommentti, setKommentti] = useState('');
+    const [arvosana, setArvosana] = useState('');
+    const [kayttaja, setKayttaja] = useState('');
+    const [user, setUser] = useState([]);
 
     useEffect(() => {
         let status = 0;
@@ -41,7 +48,59 @@ export default function Product() {
             }
           );
       }, []);
-  
+
+      useEffect(() => {
+        let status = 0;
+        fetch(URL + "comment.php", {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-type": "application/json",
+          },
+          body: JSON.stringify({
+            search: it,
+          }),
+        })
+          .then((response) => {
+            status = parseInt(response.status);
+            return response.json();
+          })
+          .then(
+            (response) => {
+              if (status === 200) {
+                setKommentit(response);
+              } else {
+                alert(response.error);
+              }
+            },
+            (error) => {
+              alert(error);
+            }
+          );
+      }, []);
+
+
+
+      const comment = (e) => {
+        axios.post('http://localhost/verkkokauppa/postcomment.php', {
+          otsikko:otsikko,
+          kommentti:kommentti,
+          arvosana:arvosana,
+          user:user[0],
+          tuoteid:it,
+        }).then((response) => {
+            console.log(response);
+            window.location.href = "http://localhost:3000/product/" + it
+        });
+    };
+
+    useEffect(() =>{
+      if("user" in localStorage){
+          setUser(JSON.parse(localStorage.getItem("user")))
+      }
+    }, [])
+
+
       useEffect(() => {
         if ("cart" in localStorage) {
           setCart(JSON.parse(localStorage.getItem("cart")));
@@ -55,13 +114,25 @@ export default function Product() {
         window.location.reload(false);
       }
 
+      function tähti (e) {
+        if (e === 4) {
+          return (<><i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i></>)
+        }  else if (e === 3) {
+          return (<><i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i></>)
+        } else if (e === 2) {
+          return (<><i class='fa fa-star'></i> <i class='fa fa-star'></i></>)
+        } else if (e === 1) {
+          return (<><i class='fa fa-star'></i></>)
+        } else {
+          return (<><i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i> <i class='fa fa-star'></i></>)
+        }
+      }
+
     return (
 <>
 
-
 <div className="row">
     <div className="bg-light">
-    
 {items.map((item) => (
         <div className="" key={item.id}>
                     
@@ -85,7 +156,58 @@ export default function Product() {
                 
                  ))}
                  </div>
-        </div>     
+        </div>    
+       
+          {kommentit.map((kommentti) => (
+            <div className="row kommenttitausta">
+        <div className="" key={kommentti.id}>
+                    
+
+                        <div className="bg-white p-2 m-2 border border-dark">
+                            <div className="">
+                            <h3 className="ms-4">{kommentti.otsikko} {tähti(parseInt(kommentti.arvosana))} </h3>
+                            <h4 className="ms-4">{kommentti.kommentti}</h4>
+                            <h5 className="ms-4">{kommentti.käyttäjä}</h5>
+                            
+                          
+                            </div>
+                        </div>
+                </div>
+                </div>
+                
+                 ))}
+                
+                 <div className="row bg-white">
+                 <div className="row">
+                   
+          <label for="examplePassword" sm={2}>Otsikko</label>
+          <div className="col-sm-10" >
+            <input onChange={(e) => setOtsikko(e.target.value)} type="text"/>
+          </div>
+        </div>
+        <div className="row">
+          <label for="examplePassword" sm={2}>Kommentti</label>
+          <div className="col-sm-10" >
+            <input onChange={(e) => setKommentti(e.target.value)} type="text"/>
+          </div>
+        </div>
+
+        <div className="row">
+          <label for="examplePassword" sm={2}>Arvosana</label>
+          <div className="col-sm-10" >
+            <input onChange={(e) => setArvosana(e.target.value)} type="text"/>
+          </div>
+        </div>
+
+        <div className="row">
+        <div className="col-sm-10" >
+            <button onClick={comment}>Lisää arvostelu</button>
+          </div>
+        </div>
+
+        </div>
+        
+        
     </>
 
     )
