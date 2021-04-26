60b105f3731100d4d6718510ab65b83f471b6162
Joni Karvonen <n9kajo01@students.oamk.fi>
2021-04-26 10:56:03 +0300

    kategoria- ja haku-sivujen ulkoasu

--- src/Category.js
+++ src/Category.js
@@ -1,10 +1,10 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 
 
-export default function Category({URL, addToCart}) {
+export default function Category({ URL, addToCart }) {
   const [search, setSearch] = useState("");
   const [tuotenimi, setTuotenimi] = useState("");
   const [hinta, setHinta] = useState("");
@@ -67,62 +67,67 @@ export default function Category({URL, addToCart}) {
   return (
     <>
       <div className="row">
-        <div className="col-3 bg-secondary border-top border-bottom border-end border-dark">
-          <h1>Haku</h1>
+        <div className="bg-secondary border-top border-dark">
+          <h1>{it}</h1>
+
           <div>
-            <select id="sortMethod" className="form-select" onChange={e => sort(e.target.value)}>
-              <option selected>Lajittele</option>
-              <option value="1">Hinta (pienin-suurin)</option>
-              <option value="2">Hinta (suurin-pienin)</option>
-              <option value="3">Uusin</option>
-              <option value="4">Vanhin</option>
-              <option value="5">Nimi (A-Ö)</option>
-              <option value="6">Nimi (Ö-A)</option>
-            </select>
+
+
+            <div className="d-flex justify-content-end mb-3">
+
+              <div className="col-3">
+                <select id="sortMethod" className="form-select" onChange={e => sort(e.target.value)}>
+                  <option selected>Lajittele</option>
+                  <option value="1">Hinta (pienin-suurin)</option>
+                  <option value="2">Hinta (suurin-pienin)</option>
+                  <option value="3">Uusin</option>
+                  <option value="4">Vanhin</option>
+                  <option value="5">Nimi (A-Ö)</option>
+                  <option value="6">Nimi (Ö-A)</option>
+                </select>
+              </div>
+            </div>
+
+
+            <ul className="row px-3">
+              {items.map((item) => (
+                <div
+                  className="card col-xl-3 col-lg-6 col-md-6 col-sm-12 text-center"
+                  key={item.id}>
+
+                  <div className="yläosa">
+                    <Link to={"/Product/" + item.id}>
+                      <img src="" className="card-img-top" alt=""></img>
+                      <div className="card-body">
+                      <img src={item.kuva} className="tuotekuva" alt="Logo" />
+                        <h5 className="card-title">{item.tuotenimi}</h5>
+                        <p className="card-text text-left">{item.tuotetiivistelmä}</p>
+                      </div>
+                    </Link>
+                  </div>
+                  <div className="row align-bottom">
+                    <div className="vasen-pohja col-6 align-bottom">
+                      <button
+                        onClick={() => addToCart(item.id)}
+                        value={item.id}
+                        className="btn btn-primary mb-2"
+                      >
+                        <i className="fa fa-shopping-cart"></i>
+                      </button>
+                    </div>
+                    <div className="oikea-pohja col-6 mt-2 align-bottom">
+                      {item.alennettuhinta ? <><del>{item.hinta + "€"}</del>
+                        <h5 className="discount">{item.alennettuhinta + "€"}</h5>
+                        <h6><span className="percent"> {"-" + Number((item.hinta - item.alennettuhinta) / item.hinta * 100).toFixed(0) + "%"}</span></h6></>
+                        : <h5 className="price">{item.hinta + "€"}</h5>}
+                    </div>
+                  </div>
+
+
+                </div>
+              ))}
+            </ul>
           </div>
-
-        </div>
-        <div className="col-9 bg-secondary border-top border-bottom border-start border-dark ">
-          <h1>Tuotteet</h1>
-
-          <ul className="row">
-          {items.map((item) => (
-        <div
-          className="card col-xl-6 col-lg-6 col-md-12 col-sm-12 text-center"
-          key={item.id}>
-          
-            <div className="yläosa">
-            <Link to={"/Product/" + item.id}>
-              <img src="" className="card-img-top" alt=""></img>
-              <div className="card-body">
-                <img src={item.kuva} className="tuotekuva" alt="Logo" />
-                <h5 className="card-title">{item.tuotenimi}</h5>
-                <p className="card-text text-left">{item.tuotetiivistelmä}</p>
-                </div>
-                </Link>
-                </div>
-                <div className="row align-bottom">
-                <div className="vasen-pohja col-6 align-bottom">
-                  <button
-                    onClick={() => addToCart(item.id)}
-                    value={item.id}
-                    className="btn btn-primary mb-2"
-                  >
-                    <i className="fa fa-shopping-cart"></i>
-                  </button>
-                </div>
-                <div className="oikea-pohja col-6 mt-2 align-bottom">
-                { item.alennettuhinta ? <><del>{item.hinta + "€"}</del>             
-                <h5 className="discount">{item.alennettuhinta + "€"}</h5>
-                <h6><span className="percent"> {"-"+ Number((item.hinta - item.alennettuhinta)/item.hinta * 100).toFixed(0) + "%"}</span></h6></>
-                :<h5 className="price">{item.hinta + "€"}</h5>}
-                </div>
-                </div>
-            
-          
-        </div>
-      ))}
-          </ul>
         </div>
       </div>
     </>
