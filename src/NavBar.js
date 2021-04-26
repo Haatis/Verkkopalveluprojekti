253c9e4ab50b20c2aeac1643fae9863b11d16be2
JoonaHaataja <joona00@windowslive.com>
2021-04-26 12:16:30 +0300

    navbar korjaus ylläpitäjänä

--- src/NavBar.js
+++ src/NavBar.js
@@ -9,19 +9,19 @@ import axios from 'axios';
 import { Link } from 'react-router-dom'
 
 
-export default function NavBar({ URL, myCart, emptyCart, user, setUser, admin, setAdmin, Logout }) {
+export default function NavBar({URL, myCart, emptyCart, user, setUser, admin, setAdmin, Logout}) {
   const [search, setSearch] = useState("")
   const [items, setItems] = useState([])
 
   const [username, setUsername] = useState('');
-
+ 
   const [password, setPassword] = useState('');
   const [loginStatus, setLoginStatus] = useState('');
 
 
   const [showOheis, setshowOheis] = useState(false);
   const [showKomp, setshowKomp] = useState(false);
-
+  
 
   const showOheisDropdown = (e) => {
     setshowOheis(!showOheis);
@@ -48,7 +48,7 @@ export default function NavBar({ URL, myCart, emptyCart, user, setUser, admin, s
     }
   }
 
-  //hakee ostoskorissa olevat tuotteet tietokannasta
+        //hakee ostoskorissa olevat tuotteet tietokannasta
   useEffect(() => {
     if ("cart" in localStorage) {
       let status = 0;
@@ -78,32 +78,32 @@ export default function NavBar({ URL, myCart, emptyCart, user, setUser, admin, s
             alert(error);
           }
         );
-    } else {
+    }else{
       setItems([])
-    }
+  }
   }, [myCart, cart])
 
-
+ 
   function emptyUser() {
 
-
-    async function logout() {
-      const config = {
-        method: 'GET',
-        credentials: 'include'
+   
+      async function logout() {
+        const config = {
+          method: 'GET',
+          credentials: 'include'
+        }
+    
+        const url = 'http://localhost/verkkokauppa/logout.php';
+        try {
+          await fetch(url,config);
+          setUser(null);
+          setAdmin(null);
+        } catch (error) {
+          alert(error);
+        }
       }
-
-      const url = 'http://localhost/verkkokauppa/logout.php';
-      try {
-        await fetch(url, config);
-        setUser(null);
-        setAdmin(null);
-      } catch (error) {
-        alert(error);
-      }
-    }
-    logout();
-
+      logout();
+  
   }
 
   return (
@@ -117,40 +117,40 @@ export default function NavBar({ URL, myCart, emptyCart, user, setUser, admin, s
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto d-flex">
               {/* <Nav.Link href="/" className="mx-1">Etusivu</Nav.Link> */}
-              <Nav.Link as={Link} to="/Class/Komponentit" className="ms-1">
+              <Nav.Link as={Link} to="/Class/komponentit" className="ms-1">
                 Komponentit
               </Nav.Link>
               <NavDropdown title="" id="collasible-nav-dropdown" className=""
                 show={showKomp}
                 onMouseEnter={showKompDropdown}
                 onMouseLeave={hideKompDropdown}>
-                <NavDropdown.Item as={Link} to="/Category/Prosessorit">
-                  Prosessorit
+                <NavDropdown.Item>
+                  <Link to="/Category/Prosessorit">Prosessorit</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Emolevyt">
-                  Emolevyt
+                <NavDropdown.Item>
+                  <Link to="/Category/Emolevyt">Emolevyt</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Näytönohjaimet">
-                  Näytönohjaimet
+                <NavDropdown.Item>
+                  <Link to="/Category/Näytönohjaimet">Näytönohjaimet</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Asemat">
-                  Asemat
+                <NavDropdown.Item>
+                  <Link to="/Category/Asemat">Asemat</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Muistit">
-                  Muistit
+                <NavDropdown.Item>
+                  <Link to="/Category/Muistit">Muistit</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Kovalevyt">
-                  Kovalevyt
+                <NavDropdown.Item>
+                  <Link to="/Category/Kovalevyt">Kovalevyt</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Jäähdytys">
-                  Jäähdytys
+                <NavDropdown.Item>
+                  <Link to="/Category/Jäähdytys">Jäähdytys</Link>
                 </NavDropdown.Item>
-                <NavDropdown.Item as={Link} to="/Category/Kotelot">
-                  Kotelot
+                <NavDropdown.Item>
+                  <Link to="/Category/Kotelot">Kotelot</Link>
                 </NavDropdown.Item>
               </NavDropdown>
               <div className="border border-dark my-0 py-0"></div>
-              <Nav.Link as={Link} to="/Class/Oheislaitteet" className="ms-1">
+              <Nav.Link as={Link} to="/Class/oheislaitteet" className="ms-1">
                 Oheislaitteet
                 </Nav.Link>
               <NavDropdown title="" id="collasible-nav-dropdown  " className="mx-1"
@@ -158,23 +158,23 @@ export default function NavBar({ URL, myCart, emptyCart, user, setUser, admin, s
                 onMouseEnter={showOheisDropdown}
                 onMouseLeave={hideOheisDropdown}>
 
-                <NavDropdown.Item as={Link} to="/Category/Näppäimistöt">
-                  Näppäimistöt
+                <NavDropdown.Item>
+                  <Link to="/Category/Näppäimistöt">Näppäimistöt</Link>
                 </NavDropdown.Item>
 
 
-                <NavDropdown.Item as={Link} to="/Category/Hiiret">
-                  Hiiret
+                <NavDropdown.Item>
+                  <Link to="/Category/Hiiret">Hiiret</Link>
                 </NavDropdown.Item>
 
 
-                <NavDropdown.Item as={Link} to="/Category/Näytöt">
-                  Näytöt
+                <NavDropdown.Item>
+                  <Link to="/Category/Näytöt">Näytöt</Link>
                 </NavDropdown.Item>
 
               </NavDropdown>
               <div className="border border-dark my-0 py-0"></div>
-              {(user) ? (<Nav.Link as={Link} to={"/Account/" + user.email} className="mx-1" href="#">Tili</Nav.Link>) : (
+              {(user) ? ( <Nav.Link as={Link} to={"/Account/" + user.email} className="mx-1" href="#">Tili</Nav.Link> ) : (
                 <Nav.Link as={Link} to="/login" className="mx-1">Tili</Nav.Link>)}
               <div className="border border-dark my-0 py-0"></div>
 
@@ -187,66 +187,69 @@ export default function NavBar({ URL, myCart, emptyCart, user, setUser, admin, s
                       {item.tuotenimi}
                     </div>
                     <div className="float-end text-danger">
-                      {item.alennettuhinta ? (counts[item.id] * item.alennettuhinta).toLocaleString("fi-FI")
-                        : (counts[item.id] * item.hinta).toLocaleString("fi-FI")} €
+                    { item.alennettuhinta ? (counts[item.id] *  item.alennettuhinta).toLocaleString("fi-FI")
+                        :    (counts[item.id] *  item.hinta).toLocaleString("fi-FI")             } €
                     </div>
                     <div className="">
                       <p><small>
-                        {counts[item.id]} X  {item.alennettuhinta ? <><del>{item.hinta + "€"}</del>
-                          <h6 className="discount">{item.alennettuhinta + "€"}</h6>
-                        </>
-                          : <h6 className="inline">{item.hinta + "€"}</h6>}
+                      {counts[item.id]} X  { item.alennettuhinta ? <><del>{item.hinta + "€"}</del>
+                <h6 className="discount">{item.alennettuhinta + "€"}</h6>
+                </>
+                :<h6 className="inline">{item.hinta + "€"}</h6>}
                       </small></p>
                     </div>
                   </NavDropdown.Item>
                 ))}
-
-                <button style={{ paddingLeft: "5px" }} className="btn btn-danger float-start col-6" type="button" onClick={() => emptyCart()}>Tyhjennä</button>
+                
+                <button style={{paddingLeft:"5px"}} className="btn btn-danger float-start col-6" type="button" onClick={() => emptyCart()}>Tyhjennä</button>
                 <Link to="/cart" className="btn btn-primary float-end col-6">
                   Kassalle
                 </Link>
-
+                
 
 
                 <Link to="/order" className="btn btn-own col-12">
-                  Siirry tilaamaan
+                Siirry tilaamaan
                 </Link>
 
 
 
               </NavDropdown>
 
-              {(user) ? (<Nav.Link onClick={() => emptyUser()} className="mx-1 ms-2 p-2"><i className="fa fa-user-alt me-2 "></i>Kirjaudu ulos <p className="käyttäjä inline-block text-truncate sposti sposti2">{user.email})</p></Nav.Link>) : (
+              {(user) ? (<Nav.Link onClick={() => emptyUser()} className="mx-1 ms-2 p-2"><i className="fa fa-user-alt me-2 "></i>Kirjaudu ulos <p className="käyttäjä inline-block text-truncate sposti">{user.email}</p></Nav.Link>) : (
                 <Nav.Link as={Link} to="/login" className="mx-1"><i className="fa fa-user-alt me-2 "></i> Kirjaudu sisään</Nav.Link>)}
 
-              {(admin) ? (<Nav.Link as={Link} to="/add" className="m-1">YP</Nav.Link>) : (
-                <Nav.Link className="hidden">YP</Nav.Link>)}
+             
+    
             </Nav>
           </Navbar.Collapse>
           <form className="float-end me-2 col-md-3 col-xl-4 col-xxl-5 d-none d-lg-flex">
-            <input className="form-control my-2" type="search" placeholder="Haku" value={search} onChange={e => setSearch(e.target.value)} />
+                <input className="form-control my-2" type="search" placeholder="Haku" value={search} onChange={e => setSearch(e.target.value)} />
 
-            <Link to={"/search?query=" + search}>
-              <button className="btn bt btn-primary my-2 py-2 text-light">
-                <i class="fas fa-search"></i>
+                <Link to={"/search?query=" + search}>
+                  <button className="btn bt btn-primary my-2 py-2 text-light">
+                  <i class="fas fa-search"></i>
               </button>
-            </Link>
+                </Link>
+                <Nav>
+                {(admin) ? (<Nav.Link as={Link} to="/add"  className="">YP</Nav.Link>) : (
+                      <Nav.Link  className="hidden">YP</Nav.Link>)}
+                      </Nav>
+              </form>
 
-          </form>
-
-
-
+              
+            
 
         </Navbar>
         <form className="d-lg-none d-flex my-2">
-          <input className="form-control " type="search" placeholder="Haku" value={search} onChange={e => setSearch(e.target.value)} />
-          <Link to={"/search?query=" + search}>
-            <button className="btn bt btn-primary text-light">
-              <i class="fas fa-search"></i>
-            </button>
-          </Link>
+                <input className="form-control " type="search" placeholder="Haku" value={search} onChange={e => setSearch(e.target.value)} />
+                <Link to={"/search?query=" + search}>
+                  <button className="btn bt btn-primary text-light">
+                  <i class="fas fa-search"></i>
+              </button>
+                </Link>
 
-        </form>
+              </form>
       </div>
     </>
   )
